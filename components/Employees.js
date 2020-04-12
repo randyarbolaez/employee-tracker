d2873ec0f439597b28321b9d691e2334bf611cdd
Randy <randyarbolaez0@gmail.com>
2020-04-12 18:17:25 -0400

    all my code

---
+++ components/Employees.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+
+import User, { CURRENT_USER_QUERY } from "./User";
+import DeleteEmployee from "./DeleteEmployee";
+
+const ALL_EMPLOYEES_QUERY = gql`
+  query ALL_EMPLOYEES_QUERY {
+    employees {
+      id
+      name
+      phoneNumber
+      email
+      salary
+      image
+      street
+      state
+      country
+      zipcode
+      user {
+        id
+      }
+    }
+  }
+`;
+
+let Table = styled.table`
+  /* display: flex;
+  justify-content: center; */
+  border-collapse: collapse;
+  width: 100%;
+`;
+
+let Img = styled.img`
+  border-radius: 100%;
+  width: 5vw;
+  /* vertical-align: middle; */
+  text-align: center;
+`;
+
+const Employees = () => (
+  <User>
+    {({ data: { me } }) => {
+      return (
+        <Query query={ALL_EMPLOYEES_QUERY}>
+          {({ data, error, loading }) => {
+            if (me == null) return null;
+            let filteredEmployees = data.employees.filter(
+              (employee) => me.id == employee.user.id
+            );
+            if (filteredEmployees.length == 0)
+              return <h1>Add an employee to get started!</h1>;
+
+            return (
+              <Table>
+                <tr>
+                  <th>Employee</th>
+                  <th>Name</th>
+                  <th>Phone Number</th>
+                  <th>E-Mail</th>
+                  <th>Salary</th>
+                  <th>Street</th>
+                  <th>State</th>
+                  <th>Country</th>
+                  <th>Zip Code</th>
+                </tr>
+                {filteredEmployees.map((employee) => {
+                  return (
+                    <tr key={employee.id}>
+                      <th>
+                        <Img
+                          src={(employee.image && employee.image) || null}
+                          alt=""
+                        />
+                      </th>
+                      <th>
+                        <span>{employee.name}</span>
+                      </th>
+                      <th>{employee.phoneNumber}</th>
+                      <th>{employee.email}</th>
+                      <th>{employee.salary}</th>
+                      <th>{employee.street}</th>
+                      <th>{employee.state}</th>
+                      <th>{employee.country}</th>
+                      <th>{employee.zipcode}</th>
+                      <th>
+                        <DeleteEmployee id={employee.id} />
+                      </th>
+                    </tr>
+                  );
+                })}
+              </Table>
+            );
+          }}
+        </Query>
+      );
+    }}
+  </User>
+);
+
+export default Employees;
+export { ALL_EMPLOYEES_QUERY };
---
+++ components/User.js
@@ -0,0 +1,20 @@
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const CURRENT_USER_QUERY = gql`
+  query {
+    me {
+      id
+      email
+      password
+    }
+  }
+`;
+const User = (props) => (
+  <Query {...props} query={CURRENT_USER_QUERY}>
+    {(payload) => props.children(payload)}
+  </Query>
+);
+
+export default User;
+export { CURRENT_USER_QUERY };
